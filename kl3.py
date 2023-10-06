d11c1e1a9490942139a1a898fe23f31ca3bb79ff
Piotr <piotr.skiera@gmail.com>
2023-10-06 09:51:39 +0200

    update

--- kl3.py
+++ kl3.py
@@ -3,19 +3,19 @@ class Car:
     Klasa samochód
     """
 
-    def __init__(self, model, year):
+    def __init__(self, model, year): # konstruktor
         self.model = model
         self.year = year
-        self.predkosc = 0
+        self.__predkosc = 0 # __ - pole prywatne
 
     def gaz(self):
-        self.predkosc += 10
+        self.__predkosc += 10
 
     def licznik(self):
-        print(f"Predkosc wynosi: {self.predkosc} km/h")
+        print(f"Predkosc wynosi: {self.__predkosc} km/h")
 
     def hamuj(self):
-        self.predkosc -= 10
+        self.__predkosc -= 10
 
 
 car1 = Car("Fiat", 2023)
@@ -24,11 +24,15 @@ car1.gaz()
 car1.gaz()
 car1.gaz()
 car1.gaz()
-print(car1.predkosc) #50 - odczytujemy pole
+# print(car1.__predkosc) #50 - odczytujemy pole
+# AttributeError: 'Car' object has no attribute '__predkosc'. Did you mean: '_Car__predkosc'?
+# znak "__" oznacza pole jako prywatne, nie odczytamy prytawnego
 car1.licznik()
 car1.hamuj()
 car1.hamuj()
 car1.hamuj()
 car1.hamuj()
 car1.hamuj()
+car1.licznik()
+car1.__predkosc = 100  # nie robimy tak
 car1.licznik()
